34f5c23f849f88267d20afe7a31ac8a1b3f4f623
Dina <dmatrosbekova@gmail.com>
2023-12-04 23:55:10 -0500

    restaurant app1

--- src/Class14/Class14_5.java
+++ src/Class14/Class14_5.java
@@ -6,10 +6,11 @@ public class Class14_5 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to Devx restaurant");
+        System.out.println("Welcome to Devx restaurant\n");
         System.out.println("Please choose one of this following options");
         System.out.println("1. Appetizers");
-        System.out.println("1. Main course");
+        System.out.println("2. Main course");
+
 
         int firstOption = sc.nextInt();
         if (firstOption == 1) {
@@ -27,7 +28,23 @@ public class Class14_5 {
             }
 
 
-        }
+        } else if(firstOption == 2) {
+            System.out.println("1. Chicken Curry");
+            System.out.println("2. New York Rib eye");
 
+            int maineCourseChoise = sc.nextInt();
+
+            if (maineCourseChoise == 1) {
+                System.out.println("Your chicken curry is being prepared bu our Chef");
+
+            } else if (maineCourseChoise == 2) {
+                System.out.println("Your New York rib eye is being grilled by our Chef");
+
+            } else {
+                System.out.println("Invalid input");
+            }
+        } else {
+            System.out.println("Wrong Choice, we have only appetizers and maincours");
+        }
     }
 }
